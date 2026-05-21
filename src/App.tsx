be3bbe47853eb7ea52fb7fb88b4329ad8a0b8b92
MOJAN3543 <sjs732@naver.com>
2026-05-21 23:31:56 +0900

    feat: OpenPark 페이지 라우팅 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/main/Main";
+import LoginPage from "./pages/login/login";
+import MainPage from "./pages/main/main";
+import RegisterPage from "./pages/register/register";
+import SelectParkingLotPage from "./pages/select/select";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex min-h-screen bg-slate-50">
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+        <Route path="/select" element={<SelectParkingLotPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
